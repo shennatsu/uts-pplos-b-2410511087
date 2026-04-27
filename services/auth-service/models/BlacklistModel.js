40700fb4b2e31d4788c4d06d3985fd224162b1d2
Alysha Ananda Shafa <aizu796@gmail.com>
2026-04-27 21:58:03 +0700

    fix: remove .env from tracking and add .env to gitignore and fix table from blacklist models

--- services/auth-service/models/BlacklistModel.js
+++ services/auth-service/models/BlacklistModel.js
@@ -2,12 +2,12 @@ const db = require('../config/db');
 
 // Pas masukin token ke blacklist pas logout
 async function blacklistToken(token) {
-    await db.execute('INSERT INTO blacklisted_tokens (token) VALUES (?)', [token]);
+    await db.execute('INSERT INTO token_blacklist (token) VALUES (?)', [token]);
 }
 
 // Cek apakah token ada di blacklist
 async function isTokenBlacklisted(token) {
-    const [rows] = await db.execute('SELECT * FROM blacklisted_tokens WHERE token = ?', [token]);
+    const [rows] = await db.execute('SELECT * FROM token_blacklist WHERE token = ?', [token]);
     return rows.length > 0; 
 }
 
